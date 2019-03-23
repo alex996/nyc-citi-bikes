e76ac82a8559fb3fce6c36721335bea6ba95ec96
Alex <alexnezhynsky@gmail.com>
2019-03-22 23:04:56 -0400

    [feat] Display a list of stations

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import data from './data.json'
 
-const App = props => (
-  <h1>Works</h1>
-)
+const App = props => {
+  const [stations, setStations] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const stations = data.stationBeanList
+      .filter(({ statusValue, testStation }) => statusValue === 'In Service' && !testStation)
+    setStations(stations)
+    setLoading(false)
+  }, [])
+
+  if (loading) {
+    return <h1>Loading...</h1>
+  }
+
+  return (
+    <>
+      <h1>Citi Bike Stations</h1>
+      <ul>
+        {stations.map(({ id, stationName, availableDocks, totalDocks }) => (
+          <li key={id}>
+            <strong>Name:</strong> {stationName} {' '}
+            <strong>Availability:</strong> {(availableDocks / totalDocks * 100).toFixed(2)}%
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
 
 export default App
