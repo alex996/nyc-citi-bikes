b55f63bd20348df0489003cf09404fe22ff2abc5
Alex <alexnezhynsky@gmail.com>
2019-03-23 12:40:33 -0400

    [feat] Fetch live data with a fallback

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from 'react'
 import debounce from 'lodash/debounce'
 import { StationList, StationPane, StationSearch } from './Stations'
 import { Loader } from './Utils'
-import data from './data.json'
+
+const apiUrl = '/stations/stations.json'
 
 const stationsFilter = ({ statusValue, testStation }) => statusValue === 'In Service' && !testStation
 
-const allStations = data.stationBeanList.filter(stationsFilter)
-
 const App = props => {
+  const [allStations, setAllStations] = useState([])
   const [stations, setStations] = useState([])
   const [loading, setLoading] = useState(true)
   const [station, setStation] = useState(null)
@@ -31,10 +31,34 @@ const App = props => {
     setStation(filtered[0])
   }
 
+  const onError = err => {
+    console.error(err)
+
+    import(/* webpackChunkName: "data" */ './data.json').then(data => {
+      const stations = data.stationBeanList.filter(stationsFilter)
+      setAllStations(stations)
+      setStations(stations)
+      setStation(stations[0])
+      setLoading(false)
+    })
+  }
+
   useEffect(() => {
-    setStations(allStations)
-    setStation(allStations[0])
-    setLoading(false)
+    fetch(apiUrl)
+      .then(res => {
+        if (res.ok) {
+          res.json().then(({ stationBeanList }) => {
+            const stations = stationBeanList.filter(stationsFilter)
+            setAllStations(stations)
+            setStations(stations)
+            setStation(stations[0])
+            setLoading(false)
+          })
+        } else {
+          onError(res)
+        }
+      })
+      .catch(onError)
   }, [])
 
   if (loading) {
